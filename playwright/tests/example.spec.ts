0d20c416f00498909c6c308b6553a2ebd03fcbfc
Matti <wasabi82@gmail.com>
2022-03-29 22:02:18 +0300

    width increased

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -1,15 +1,15 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-  await page.setViewportSize({ width: 1600, height: 1200 });
-  await page.goto('https://kungfo0.github.io/pokeri-ui/');
-});
+  await page.setViewportSize({ width: 1700, height: 1200 })
+  await page.goto('https://kungfo0.github.io/pokeri-ui/')
+})
 
 test.describe('Screenshot rounds', () => {
   test('should login and take a screenshot', async ({ page }) => {
-    await page.locator('input:first-child').fill(process.env.POKERI_USER);
-    await page.locator('input:last-child').fill(process.env.POKERI_PASS);
-    await page.locator('button:first-child').click();
+    await page.locator('input:first-child').fill(process.env.POKERI_USER)
+    await page.locator('input:last-child').fill(process.env.POKERI_PASS)
+    await page.locator('button:first-child').click()
 
     await page.waitForSelector('table')
     // await page.waitForSelector('text=previous')
@@ -17,7 +17,7 @@ test.describe('Screenshot rounds', () => {
 
     await expect(page.locator('data-test-id=rounds-for-season')).toBeVisible()
 
-    await page.locator('data-test-id=rounds-for-season').screenshot({ path: 'rounds.png'});
-    await page.locator('data-test-id=totals').screenshot({ path: 'totals.png'});
-  });
-});
+    await page.locator('data-test-id=rounds-for-season').screenshot({ path: 'rounds.png'})
+    await page.locator('data-test-id=totals').screenshot({ path: 'totals.png'})
+  })
+})
